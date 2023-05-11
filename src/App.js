469693f94a7ead57a29681711230855b14a26bfe
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-05-11 18:04:10 -0500

    this commit edited the skeleton layout to include my javascript projects from my 50 day challenge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import 'bulma/css/bulma.css'
 import ProfileCard from "./ProfileCard";
-import AlexaImage from './images/alexa.png';
-import CortanaImage from './images/cortana.png';
-import SiriImage from './images/siri.png';
+import ExpandingCardsGif from './gifs/expanding_cards.gif';
+import ProgressStepsGif from './gifs/progress_steps.gif';
+import RotatingNavigationGif from './gifs/rotating_navigation.gif';
+import HiddenSearchWidget from './gifs/hidden_search_widget.gif';
 
 
 function App(){
@@ -16,39 +17,41 @@ function App(){
 
         <div className="container">
           <section className="section">
-            <div className="columns">
-              <div className="column is-4">
-                <ProfileCard
-                    title="Project 1: Expanding Cards"
-                    image={AlexaImage}
-                    description="- used Javascript, CSS, and HTML
-- cards will display small on the screen with a background image when not clicked
-- cards will expand and a heading will appear with a css transition when clicked on"
-                />
-              </div>
-              <div className="column is-4">
-                <ProfileCard
-                    title="Project 2: Progress Steps"
-                    image={CortanaImage}
-                    description="- used Javascript, CSS, and HTML
-- clicking the next button transitions the circles to the next number
-- when we get to the last one the next button becomes disabled
-- when we get to the first one the previous button becomes disabled"
-                />
-              </div>
-              <div className="column is-4">
-                <ProfileCard
-                    title="Project 3: Rotating Navbar"
-                    image={SiriImage}
-                    description="- used Javascript, CSS, and HTML
+            <div className="rows">
+                <div className="columns">
+                    <div className="column is-4">
+                      <ProfileCard
+                          title="Project 1: Expanding Cards"
+                          image={ExpandingCardsGif}
+                          description="- used Javascript, CSS, and HTML
+      - cards will display small on the screen with a background image when not clicked
+      - cards will expand and a heading will appear with a css transition when clicked on"
+                      />
+                    </div>
+                    <div className="column is-4">
+                      <ProfileCard
+                          title="Project 2: Progress Steps"
+                          image={ProgressStepsGif}
+                          description="- used Javascript, CSS, and HTML
+      - clicking the next button transitions the circles to the next number
+      - when we get to the last one the next button becomes disabled
+      - when we get to the first one the previous button becomes disabled"
+                      />
+                  </div>
+                    <div className="column is-4">
+                        <ProfileCard
+                            title="Project 3: Rotating Navbar"
+                            image={RotatingNavigationGif}
+                            description="- used Javascript, CSS, and HTML
 - once you click on the navigation icon, the page rotates and the navigation appears at the bottom of the page
 - the icon also changes to a close button once the navigation appears"
-                />
-              </div>
+                        />
+                    </div>
+                </div>
               <div className="column is-4">
                 <ProfileCard
                     title="Project 4: Hidden Search Widget"
-                    image={SiriImage}
+                    image={HiddenSearchWidget}
                     description="- used Javascript, CSS, and HTML
 - when you toggle the search icon the search field expands and collapses"
                 />
